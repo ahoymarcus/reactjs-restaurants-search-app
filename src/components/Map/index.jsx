ea7371490ecd0c834b8511b9d5e82509c6c1d771
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-07-13 11:36:13 -0300

    Inserir os Markers do Google Maps no componente Map da app.

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { GoogleApiWrapper, Map, Marker } from 'google-maps-react'
 
@@ -9,6 +9,7 @@ import { setRestaurants } from '../../redux/modules/restaurants';
 
 export const MapContainer = (props) => {
 	const dispatch = useDispatch();
+	const {restaurants } = useSelector((state) => state.restaurants);
 	const [ map, setMap ] = useState(null);
 	const { google, query } = props;
 	
@@ -62,7 +63,14 @@ export const MapContainer = (props) => {
 	
 	
 	return (
-		<Map google={google} centerAroundCurrentLocation onReady={onMapReady} onRecenter={onMapReady} />
+		<Map google={google} centerAroundCurrentLocation onReady={onMapReady} onRecenter={onMapReady}>
+			{restaurants.map((restaurant) => (
+				<Marker key={restaurant.place_id} name={restaurant.name} position={{
+					lat: restaurant.geometry.location.lat(),
+					lng: restaurant.geometry.location.lng(),
+				}} />
+			))}
+		</Map>
 	);
 };
 
